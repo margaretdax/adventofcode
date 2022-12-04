9759d42e89b91a4920fbbc80411562595f236c4f
Margaret Dax <margaretdax@gmail.com>
2022-12-04 08:46:27 +0100

    make cargo check happy, add template for copying

--- problems/src/solvers/pyynn.rs
+++ problems/src/solvers/pyynn.rs
@@ -5,13 +5,12 @@ pub struct Solver;
 impl Solution for Solver {
 	fn solve(self: Box<Self>, lines: Vec<String>) -> String {
 		let mut solution = "".to_owned();
-		for _line in lines.iter() {
+		for line in lines.iter() {
 
 		}
 		solution.push_str(format!("Part I: {}\n", 0).as_str());
-
 		
-		for _line in lines.iter() {
+		for line in lines.iter() {
 
 		}
 		solution.push_str(format!("Part II: {}\n", 0).as_str());
