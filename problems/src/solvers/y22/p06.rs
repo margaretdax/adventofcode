71bea03826852a2c3af663ebe00b173a330ef4fc
Margaret Dax <margaretdax@gmail.com>
2022-12-06 07:50:52 +0100

    6.1

--- problems/src/solvers/y22/p06.rs
+++ problems/src/solvers/y22/p06.rs
@@ -3,20 +3,46 @@ use utils::*;
 pub struct Solver;
 
 impl Solution for Solver {
-    fn get_number(self: &Self) -> i32 { 5 }
+    fn get_number(self: &Self) -> i32 { 6 }
 
 	fn solve(self: &Self, lines: Vec<String>) -> Option<String> {
 		let mut solution = "".to_owned();
-		for _line in lines.iter() {
-			
+		let mut answer = "".to_owned();
+		for line in lines.iter() {
+			let marker = find_marker(line);
+			answer.push_str(format!("{}, ", marker).as_str());
 		}
-		solution.push_str(format!("Part I: {}\n", 0).as_str());
+		solution.push_str(format!("Part I: {}\n", answer).as_str());
 
 		for _line in lines.iter() {
 			
 		}
 		solution.push_str(format!("Part II: {}\n", 0).as_str());
 		
-		None
+		Some(solution)
 	}
+}
+
+fn find_marker(s: &String) -> usize {
+	let end = s.len() - 4;
+	for i in 0..=end {
+		let sub = &s[i..=i+3];
+		if has_dup(sub) {
+			return i+4
+		}		
+	}
+	
+	0
+}
+
+fn has_dup(s: &str) -> bool {
+	let bytes = s.as_bytes();
+	for i in 0..s.len() {
+		for j in i+1..s.len() {
+			if bytes[i] == bytes[j] {
+				return false
+			}
+		}
+	}
+	return true
 }
